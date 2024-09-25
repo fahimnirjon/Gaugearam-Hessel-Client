e688d2b0870c5c208bd6746891425b91b1a88c1f
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-25 23:17:33 +0600

    added

--- src/Pages/Recommand.jsx
+++ src/Pages/Recommand.jsx
@@ -1,48 +1,52 @@
 import React from "react";
 import img from "../assets/menu/salad-bg.jpg";
+import SectionTitle from "../Components/SectionTitle/SectionTitle";
 
 const Recommand = () => {
   return (
-    <div className="flex gap-10 lg:items-center justify-center">
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img src={img} alt="salad" className="rounded-xl" />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Caesar Salad</h2>
-          <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
-          <div className="card-actions">
-            <button className="btn btn-outline border-0 border-b-2 border-yellow-500 uppercase">
-              add to cart
-            </button>
+    <div>
+        <SectionTitle heading={'chef recommends'} subHeading={'Should Try'}></SectionTitle>
+      <div className="flex gap-10 lg:items-center justify-center">
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img src={img} alt="salad" className="rounded-xl" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Caesar Salad</h2>
+            <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+            <div className="card-actions">
+              <button className="btn btn-outline border-0 border-b-2 border-yellow-500 uppercase">
+                add to cart
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img src={img} alt="salad" className="rounded-xl" />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Caesar Salad</h2>
-          <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
-          <div className="card-actions">
-            <button className="btn btn-outline border-0 border-b-2 border-yellow-500 uppercase">
-              add to cart
-            </button>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img src={img} alt="salad" className="rounded-xl" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Caesar Salad</h2>
+            <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+            <div className="card-actions">
+              <button className="btn btn-outline border-0 border-b-2 border-yellow-500 uppercase">
+                add to cart
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img src={img} alt="salad" className="rounded-xl" />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Caesar Salad</h2>
-          <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
-          <div className="card-actions">
-            <button className="btn btn-outline border-0 border-b-2 border-yellow-500 uppercase">
-              add to cart
-            </button>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img src={img} alt="salad" className="rounded-xl" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Caesar Salad</h2>
+            <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+            <div className="card-actions">
+              <button className="btn btn-outline border-0 border-b-2 border-yellow-500 uppercase">
+                add to cart
+              </button>
+            </div>
           </div>
         </div>
       </div>
