e76f9c638284edf0bec78f515934052b49dbcf03
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-26 01:12:07 +0600

    added

--- src/Components/SocialTab.jsx
+++ src/Components/SocialTab.jsx
@@ -4,15 +4,24 @@ import { FaGithub } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 import { SiLinkedin } from 'react-icons/si';
 import { SocialIcon } from 'react-social-icons';
+import useAuth from '../hooks/useAuth';
 
 const SocialTab = () => {
+    const {googleSignIn} = useAuth();
+
+    const handleGoogle = () =>{
+        googleSignIn()
+        .then(result=> {
+            console.log(result.user)
+        })
+    }
     return (
         <div>
             <p className='text-center mb-4 text-sm font-medium'>Or sign in with</p>
             <div className='divider'></div>
             
             <div className='justify-center gap-4 text-3xl flex mb-5'>
-                <button className=''>
+                <button onClick={handleGoogle} className=''>
                 <FcGoogle />
                 </button>
                 <button className=''>
