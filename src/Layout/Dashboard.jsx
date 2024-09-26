b5fc806c58e4c3e66181b6245035c43ee8b7a7d1
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-26 21:40:00 +0600

    added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -6,7 +6,9 @@ import {
   FaCalendar,
   FaHome,
   FaIdCard,
+  FaList,
   FaShoppingCart,
+  FaUtensils,
 } from "react-icons/fa";
 import { FaShop } from "react-icons/fa6";
 import { IoMdContact } from "react-icons/io";
@@ -15,48 +17,94 @@ import { TbBrandBooking } from "react-icons/tb";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
+  const isAdmin = true;
+
   return (
     <div className="flex">
       {/* side bar */}
       <div className="w-64 min-h-screen bg-orange-400">
         <ul className="menu">
-          <li>
-            <NavLink className="gap-3" to="/dashboard/userhome">
-              <FaHome></FaHome>
-              User Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="gap-3" to="/dashboard/reservation">
-              <FaCalendar></FaCalendar>
-              Reservation
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="gap-3" to="/dashboard/payment">
-              <FaIdCard></FaIdCard>
-              Payment History
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="gap-3" to="/dashboard/cart">
-            <AiOutlineShoppingCart />
-              My Cart
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="gap-3" to="/dashboard/review">
-              <MdReviews />
-              ADD Review
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink className="gap-3" to="/dashboard/bookings">
-              <TbBrandBooking />
-              My Booking
-            </NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+              {" "}
+              <li>
+                <NavLink className="gap-3" to="/dashboard/adminhome">
+                  <FaHome></FaHome>
+                  Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="gap-3" to="/dashboard/additems">
+                  <FaUtensils></FaUtensils>
+                  Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="gap-3" to="/dashboard/manageitem">
+                  <FaList></FaList>
+                  Manage Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="gap-3" to="/dashboard/cart">
+                  <AiOutlineShoppingCart />
+                  My Cart
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="gap-3" to="/dashboard/review">
+                  <MdReviews />
+                  ADD Review
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="gap-3" to="/dashboard/bookings">
+                  <TbBrandBooking />
+                  My Booking
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              {" "}
+              <li>
+                <NavLink className="gap-3" to="/dashboard/userhome">
+                  <FaHome></FaHome>
+                  User Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="gap-3" to="/dashboard/reservation">
+                  <FaCalendar></FaCalendar>
+                  Reservation
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="gap-3" to="/dashboard/payment">
+                  <FaIdCard></FaIdCard>
+                  Payment History
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="gap-3" to="/dashboard/cart">
+                  <AiOutlineShoppingCart />
+                  My Cart
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="gap-3" to="/dashboard/review">
+                  <MdReviews />
+                  ADD Review
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="gap-3" to="/dashboard/bookings">
+                  <TbBrandBooking />
+                  My Booking
+                </NavLink>
+              </li>
+            </>
+          )}
           <div className="divider"></div>
           <li>
             <NavLink className="gap-3" to="/">
@@ -66,19 +114,19 @@ const Dashboard = () => {
           </li>
           <li>
             <NavLink className="gap-3" to="/menu">
-            <MdMenuBook />
+              <MdMenuBook />
               Menu
             </NavLink>
           </li>
           <li>
             <NavLink className="gap-3" to="/order">
-            <FaShop />
+              <FaShop />
               Shop
             </NavLink>
           </li>
           <li>
             <NavLink className="gap-3" to="/contact">
-            <IoMdContact />
+              <IoMdContact />
               Contact
             </NavLink>
           </li>
