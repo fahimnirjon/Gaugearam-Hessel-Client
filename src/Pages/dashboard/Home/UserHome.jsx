3c8039ae784015c451f2b4204d36a75fa482495f
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:33:24 +0600

    added

--- src/Pages/dashboard/Home/UserHome.jsx
+++ src/Pages/dashboard/Home/UserHome.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import useAuth from '../../../hooks/useAuth';
+import React from "react";
+import useAuth from "../../../hooks/useAuth";
 
 const UserHome = () => {
-    const {user} = useAuth()
-    return (
-        <div>
-            <p className='text-3xl'>Hi, Welcome {
-                user?.displayName ? user.displayName : 'Back!'
-            }</p>
-            
-        </div>
-    );
+  const { user } = useAuth();
+  return (
+    <div>
+      <p className="text-3xl">
+        Hi, Welcome {user?.displayName ? user.displayName : "Back!"}
+      </p>
+    </div>
+  );
 };
 
 export default UserHome;
