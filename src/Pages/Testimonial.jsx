d171be9838f8583d694a34c35f66bb64e7a77882
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:41:54 +0600

    added

--- src/Pages/Testimonial.jsx
+++ src/Pages/Testimonial.jsx
@@ -5,8 +5,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { Rating } from "@smastrom/react-rating";
-import '@smastrom/react-rating/style.css'
-import ico from '../assets/icon/quote-left 1.png'
+import "@smastrom/react-rating/style.css";
+import ico from "../assets/icon/quote-left 1.png";
 
 const Testimonial = () => {
   const [reviews, setReviews] = useState([]);
@@ -22,12 +22,22 @@ const Testimonial = () => {
         subHeading={"What Our Client Says"}
         heading={"testimonials"}
       ></SectionTitle>
-      <Swiper autoplay={true} navigation={true} modules={[Navigation]} className="mySwiper">
+      <Swiper
+        autoplay={true}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
         {/* <SwiperSlide></SwiperSlide> */}
         {reviews.map((review) => (
           <SwiperSlide key={review._id}>
             <div className="flex flex-col items-center mx-24 my-10 space-y-6">
-              <Rating className="mb-5" style={{ maxWidth: 180 }} value={review.rating}  readOnly/>
+              <Rating
+                className="mb-5"
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
               <img className="" src={ico} alt="" />
               <p className="text-xl">{review.details}</p>
               <h3 className="text-2xl text-orange-400">{review.name}</h3>
