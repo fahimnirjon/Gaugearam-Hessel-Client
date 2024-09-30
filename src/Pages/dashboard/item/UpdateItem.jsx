5f113bbe3bdfe0e77988b4f009fcd86c185eac13
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:35:34 +0600

    added

--- src/Pages/dashboard/item/UpdateItem.jsx
+++ src/Pages/dashboard/item/UpdateItem.jsx
@@ -5,47 +5,46 @@ import { useForm } from "react-hook-form";
 import useAxiosOpen from "../../../hooks/useAxiosOpen";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
-const img_hosting_key= import.meta.env.VITE_image_hosting_key;
+const img_hosting_key = import.meta.env.VITE_image_hosting_key;
 
-const img_hosting_api =  `https://api.imgbb.com/1/upload?key=${img_hosting_key}`
+const img_hosting_api = `https://api.imgbb.com/1/upload?key=${img_hosting_key}`;
 
 const UpdateItem = () => {
-    const {name, category, recipe, price, _id} = useLoaderData();
-    const { register, handleSubmit, reset } = useForm();
-    const axiosOpen = useAxiosOpen();
+  const { name, category, recipe, price, _id } = useLoaderData();
+  const { register, handleSubmit, reset } = useForm();
+  const axiosOpen = useAxiosOpen();
   const axiosSecure = useAxiosSecure();
-  const onSubmit = async(data) => {
+  const onSubmit = async (data) => {
     console.log(data);
     // image
-    const imgFile = {image: data.image[0]}
+    const imgFile = { image: data.image[0] };
     const res = await axiosOpen.post(img_hosting_api, imgFile, {
-        headers: {
-           'content-type' : 'multipart/form-data'
-        }
+      headers: {
+        "content-type": "multipart/form-data",
+      },
     });
-    if(res.data.success){
-        const menuItem= {
-            name : data.name,
-            category: data.category,
-            price: parseFloat(data.price),
-            recipe: data.recipe,
-            image: res.data.data.display_url
-        }
-        // send data via admin
-        const menuRes = await axiosSecure.patch(`/menu/${_id}`, menuItem);
-        console.log(menuRes.data)
-        if(menuRes.data.modifiedCount > 0){
-           
-            Swal.fire({
-                position: "center",
-                icon: "success",
-                title: `${data.name} has been saved successfully!`,
-                showConfirmButton: false,
-                timer: 1500
-              });
-        }
+    if (res.data.success) {
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      };
+      // send data via admin
+      const menuRes = await axiosSecure.patch(`/menu/${_id}`, menuItem);
+      console.log(menuRes.data);
+      if (menuRes.data.modifiedCount > 0) {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: `${data.name} has been saved successfully!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
     }
-    console.log(res.data)
+    console.log(res.data);
   };
   return (
     <div>
@@ -58,7 +57,7 @@ const UpdateItem = () => {
             </div>
           </div>
           <input
-          defaultValue={name}
+            defaultValue={name}
             type="text"
             placeholder="Recipe Name"
             className="input input-bordered w-full "
@@ -73,7 +72,6 @@ const UpdateItem = () => {
                 </div>
               </div>
               <select
-              
                 defaultValue={category}
                 {...register("category", { required: true })}
                 className="select select-bordered w-full"
@@ -96,7 +94,7 @@ const UpdateItem = () => {
                 </div>
               </div>
               <input
-              defaultValue={price}
+                defaultValue={price}
                 type="number"
                 placeholder="price"
                 className="input input-bordered w-full "
@@ -109,8 +107,7 @@ const UpdateItem = () => {
               <span className="label-text">Recipe Details*</span>
             </div>
             <textarea
-            
-            defaultValue={recipe}
+              defaultValue={recipe}
               className="textarea textarea-bordered h-24"
               {...register("recipe", { required: true })}
               placeholder="recipe details"
@@ -123,9 +120,7 @@ const UpdateItem = () => {
               className="file-input file-input-ghost w-full max-w-xs"
             />
           </div>
-          <button className="btn uppercase">
-            updated menu item 
-          </button>
+          <button className="btn uppercase">updated menu item</button>
         </form>
       </div>
     </div>
