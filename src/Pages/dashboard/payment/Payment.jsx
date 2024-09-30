1fe72cdcbe8233565e5ad55346a87d10a3239d10
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:36:07 +0600

    added

--- src/Pages/dashboard/payment/Payment.jsx
+++ src/Pages/dashboard/payment/Payment.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import CheckOutForm from './CheckOutForm';
+import React from "react";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "./CheckOutForm";
 
-const stripePromise = loadStripe(import.meta.env.VITE_Payment_pk)
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_pk);
 
 const Payment = () => {
-    return (
-        <div>
-            <SectionTitle heading='payment' subHeading='Please Pay To Eat '></SectionTitle>
-            <div>
-                <Elements stripe={stripePromise}>
-                    <CheckOutForm></CheckOutForm>
-                </Elements>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <SectionTitle
+        heading="payment"
+        subHeading="Please Pay To Eat "
+      ></SectionTitle>
+      <div>
+        <Elements stripe={stripePromise}>
+          <CheckOutForm></CheckOutForm>
+        </Elements>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
