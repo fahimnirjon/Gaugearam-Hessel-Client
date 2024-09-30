2b9e41729775c07570c97b021807ae06840a15c6
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:40:52 +0600

    added

--- src/Pages/PopularMenu.jsx
+++ src/Pages/PopularMenu.jsx
@@ -1,4 +1,4 @@
-import React,  useEffect  from "react";
+import React from "react";
 import SectionTitle from "../Components/SectionTitle/SectionTitle";
 import MenuItem from "./Shared/MenuItem";
 import useMenu from "../hooks/useMenu";
