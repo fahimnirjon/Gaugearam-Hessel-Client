bbefb82085facdf5c400e29834c4cd83572e1dcb
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:26:18 +0600

    added

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,18 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosOpen from "./useAxiosOpen";
 
-const useMenu = () =>{
-    const axiosOpen = useAxiosOpen();
+const useMenu = () => {
+  const axiosOpen = useAxiosOpen();
 
-    const {data : menu = [], isPending: loading, refetch} = useQuery({
-        queryKey: ['menu'],
-        queryFn: async()=>{
-            const res = await axiosOpen.get('/menu')
-            return res.data;
-        }
-    })
+  const {
+    data: menu = [],
+    isPending: loading,
+    refetch,
+  } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const res = await axiosOpen.get("/menu");
+      return res.data;
+    },
+  });
 
-    return [menu, loading, refetch]
-}
+  return [menu, loading, refetch];
+};
 
 export default useMenu;
