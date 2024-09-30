876f8bc5fd680f81b5b18150c6cd8d4f5f916658
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:27:40 +0600

    added

--- src/hooks/IsAdmin.jsx
+++ src/hooks/IsAdmin.jsx
@@ -3,17 +3,17 @@ import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 
 const IsAdmin = () => {
-    const {user, loading} = useAuth();
-    const axiosSecure = useAxiosSecure();
-    const {data: isAdmin, isPending: isAdminLoading} = useQuery({
-        enabled: !loading,
-        queryKey: [user?.email, 'isAdmin'],
-        queryFn: async()=>{
-            const res = await axiosSecure.get(`/users/admin/${user.email}`)
-            return res.data?.admin;
-        }
-    })
-    return [isAdmin, isAdminLoading]
+  const { user, loading } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+    enabled: !loading,
+    queryKey: [user?.email, "isAdmin"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/admin/${user.email}`);
+      return res.data?.admin;
+    },
+  });
+  return [isAdmin, isAdminLoading];
 };
 
 export default IsAdmin;
