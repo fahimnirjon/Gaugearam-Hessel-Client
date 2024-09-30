318fd54fcb3df7bad7c33f5cccd2fed166711b4b
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:31:28 +0600

    added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import Footer from '../Pages/Shared/Footer';
-import Navbar from '../Pages/Shared/Navbar';
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer";
+import Navbar from "../Pages/Shared/Navbar";
 
 const Main = () => {
-    const location = useLocation();
-    const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('signup');
-    return (
-        <div>
-            {noHeaderFooter || <Navbar></Navbar>}
-            <Outlet></Outlet>
-            {noHeaderFooter || <Footer></Footer>}
-        </div>
-    );
+  const location = useLocation();
+  const noHeaderFooter =
+    location.pathname.includes("login") || location.pathname.includes("signup");
+  return (
+    <div>
+      {noHeaderFooter || <Navbar></Navbar>}
+      <Outlet></Outlet>
+      {noHeaderFooter || <Footer></Footer>}
+    </div>
+  );
 };
 
 export default Main;
