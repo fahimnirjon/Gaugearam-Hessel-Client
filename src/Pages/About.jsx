7f6bd3fa4bcc6d8cbefc0690ce1e83e3787f06d4
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:38:44 +0600

    added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import '../Pages/Featured/About.css'
-
+import "../Pages/Featured/About.css";
 
 const About = () => {
   return (
     <div>
-      <div
-        className="hero about"
-      >
+      <div className="hero about">
         <div className="hero-overlay bg-opacity-70"></div>
         <div className="hero-content text-center">
           <div className="lg:w-[1000px] md:w-[600px] bg-white m-20 rounded-2xl p-5">
