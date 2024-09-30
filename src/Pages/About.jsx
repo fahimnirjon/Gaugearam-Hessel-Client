b43156bc280f5da61e2751aa09b0a639a45dd9b1
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:45:08 +0600

    added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -10,8 +10,7 @@ const About = () => {
           <div className="lg:w-[1000px] md:w-[600px] bg-white m-20 rounded-2xl p-5">
             <h1 className="mb-5 text-5xl font-bold ">Gaogeram Hessel</h1>
             <p className="mb-5 px-16">
-              
-              
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, soluta similique! Ad a eum, impedit id deleniti voluptatibus tempore cum? Odio sint nesciunt omnis distinctio.
             </p>
           </div>
         </div>
