6c4dd56ca3024eff37915eb278f10eafd63fd335
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:39:00 +0600

    added

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Carousel } from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import img1 from "../assets/home/01.jpg";
 import img2 from "../assets/home/02.jpg";
 import img3 from "../assets/home/03.png";
@@ -10,34 +10,24 @@ import img6 from "../assets/home/06.png";
 
 const Banner = () => {
   return (
-    <Carousel
-      showArrows={true}
-      autoPlay= {Boolean}
-      infiniteLoop={Boolean}  
-    >
+    <Carousel showArrows={true} autoPlay={Boolean} infiniteLoop={Boolean}>
       <div>
         <img src={img1} />
-        
       </div>
       <div>
         <img src={img2} />
-       
       </div>
       <div>
         <img src={img3} />
-        
       </div>
       <div>
         <img src={img4} />
-        
       </div>
       <div>
         <img src={img5} />
-       
       </div>
       <div>
         <img src={img6} />
-        
       </div>
     </Carousel>
   );
