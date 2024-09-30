7c614bf89eb80247f23eb6cbcc42053f8fa8f490
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-30 15:28:48 +0600

    added

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -1,12 +1,10 @@
-
-
-const SectionTitle = ({heading, subHeading}) => {
-    return (
-        <div className="mx-auto text-center md:w-4/12 my-8">
-            <p className="text-yellow-500 mb-2 italic">~~ {subHeading} ~~</p>
-            <p className="text-4xl uppercase border-y-4 py-3">{heading}</p>
-        </div>
-    );
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div className="mx-auto text-center md:w-4/12 my-8">
+      <p className="text-yellow-500 mb-2 italic">~~ {subHeading} ~~</p>
+      <p className="text-4xl uppercase border-y-4 py-3">{heading}</p>
+    </div>
+  );
 };
 
 export default SectionTitle;
